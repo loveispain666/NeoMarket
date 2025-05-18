9dfc5776d4bb1f8db1ccf4bcdaa79f5a6f923b78
unknown <love is pain>
2025-05-18 14:53:59 +0200

    first commit

---
+++ neomarket/src/Router.js
@@ -0,0 +1,9 @@
+import ItemCard from "./components/ItemCard/ItemCard";
+
+function Router() {
+  return (
+    <ItemCard title='Doll' desc='agdflgjkldfgjkldfk.' price='5,99' imgName='Cat.jpg' />
+  )
+}
+
+export default Router;
